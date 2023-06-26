61222d3b1f153fbe97f9e58298a0add34f7eaffa
Paulo Henrique Santana de Oliveira <psoliveira150@gmail.com>
2023-06-26 09:36:23 -0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chat Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
