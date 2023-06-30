260dab267551dd8786c7be71e0a30653f6a7f984
Paulo Henrique Santana de Oliveira <psoliveira150@gmail.com>
2023-06-30 17:29:19 -0300

    initial commit

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:chatflutterv2/text_composer.dart';
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart' hide Action;
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -9,14 +13,63 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  Future<void> _sendMessege({String? text, File? imgFile}) async {
+    Map<String, dynamic> data = {};
+
+    if (imgFile != null) {
+      UploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imgFile);
+
+      TaskSnapshot taskSnapshot = await task;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imgUrl'] = url;
+    }
+
+    if (text != null) data['text'] = text;
+    FirebaseFirestore.instance.collection("messages").add(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Olá"),
+        title: const Text("Olá"),
+        centerTitle: true,
         elevation: 0,
       ),
-      body: const TextComposer(),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream:
+                  FirebaseFirestore.instance.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  default:
+                    List<DocumentSnapshot> documents =
+                        snapshot.data!.docs.reversed.toList();
+                    return ListView.builder(
+                        reverse: true,
+                        itemCount: documents.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(documents[index].get('text')),
+                          );
+                        });
+                }
+              },
+            ),
+          ),
+          TextComposer((_sendMessege)),
+        ],
+      ),
     );
   }
 }
--- lib/text_composer.dart
+++ lib/text_composer.dart
@@ -1,7 +1,13 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+// ignore: must_be_immutable
 class TextComposer extends StatefulWidget {
-  const TextComposer({super.key});
+  TextComposer(this.sendMessage, {super.key});
+
+  final Function({String? text, File? imgFile})? sendMessage;
 
   @override
   State<TextComposer> createState() => _TextComposerState();
@@ -9,6 +15,14 @@ class TextComposer extends StatefulWidget {
 
 class _TextComposerState extends State<TextComposer> {
   bool _isComposing = false;
+  final TextEditingController _controller = TextEditingController();
+
+  void reset() {
+    _controller.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +30,21 @@ class _TextComposerState extends State<TextComposer> {
       margin: EdgeInsets.symmetric(horizontal: 8),
       child: Row(
         children: <Widget>[
-          const IconButton(
-            icon: Icon(Icons.photo_camera),
-            onPressed: (vazio),
+          IconButton(
+            onPressed: () async {
+              final XFile? imgFile =
+                  await ImagePicker().pickImage(source: ImageSource.camera);
+              if (imgFile == null) {
+                return;
+              }
+              File fileSend = File(imgFile.path);
+              widget.sendMessage!(imgFile: fileSend);
+            },
+            icon: const Icon(Icons.photo_camera),
           ),
           Expanded(
             child: TextField(
+              controller: _controller,
               decoration:
                   const InputDecoration.collapsed(hintText: "Enviar Mensagem"),
               onChanged: (text) {
@@ -29,12 +52,20 @@ class _TextComposerState extends State<TextComposer> {
                   _isComposing = text.isNotEmpty;
                 });
               },
-              onSubmitted: (vazio) {},
+              onSubmitted: (text) {
+                widget.sendMessage!(text: text);
+                reset();
+              },
             ),
           ),
           IconButton(
             icon: const Icon(Icons.send),
-            onPressed: _isComposing ? () {} : null,
+            onPressed: _isComposing
+                ? () {
+                    widget.sendMessage!(text: _controller.text);
+                    reset();
+                  }
+                : null,
           )
         ],
       ),
